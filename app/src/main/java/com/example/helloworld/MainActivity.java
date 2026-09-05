764de36663e75f83fb5da8a9dec9beb9e7d97f68
zacito <43497336+zacito@users.noreply.github.com>
2026-09-05 08:39:08 -0500

    Initial Android Hello World app setup

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        TextView helloText = findViewById(R.id.hello_text);
+        helloText.setText("Hello World!");
+    }
+}
